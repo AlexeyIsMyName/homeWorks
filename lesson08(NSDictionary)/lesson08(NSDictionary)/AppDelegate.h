0cb9f4e433ef317eace159f00d216177d36ce28c
Aleksey <suslov.aleksey.oo7@gmail.com>
2019-04-16 22:16:55 +0300

    homework08

---
+++ lesson08(NSDictionary)/lesson08(NSDictionary)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lesson08(NSDictionary)
+//
+//  Created by Алексей on 16/04/2019.
+//  Copyright © 2019 Aleksey Suslov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
