133dcc70d2c44575206473fc35bbdb67d8dd264d
Aleksey <suslov.aleksey.oo7@gmail.com>
2019-04-24 07:08:48 +0300

    level student done

--- homework09(Delegates)/homework09(Delegates)/Doctor.m
+++ homework09(Delegates)/homework09(Delegates)/Doctor.m
@@ -13,17 +13,16 @@
 - (void)patientCure:(nonnull Patient *)patient {
     
     switch (patient.symptom) {
-        case cough: NSLog(@"1cough %@", patient.name); break;
-        case runnyNose: NSLog(@"2runnyNose %@", patient.name); break;
-        case soreThroat: NSLog(@"3soreThroat %@", patient.name); break;
-        case stomachAche: NSLog(@"4stomachAche %@", patient.name); break;
-        case headAche: NSLog(@"5headAche %@", patient.name); break;
-        case temperature: NSLog(@"6temperature %@", patient.name); break;
-        case none: NSLog(@"7none %@", patient.name); break;
+        case cough: NSLog(@"Doc said: %@ take a cough pill ", patient.name); break;
+        case runnyNose: NSLog(@"Doc said: %@ take a drops from the common cold", patient.name); break;
+        case soreThroat: NSLog(@"Doc said: %@ take a throat spray", patient.name); break;
+        case stomachAche: NSLog(@"Doc said: %@ buy Mezim", patient.name); break;
+        case headAche: NSLog(@"Doc said: %@ head not ass", patient.name); break;
+        case temperature: NSLog(@"Doc said: %@ take aspirin", patient.name); break;
+        case none: NSLog(@"Doc said: %@ go home, you are not sick", patient.name); break;
         default:
             break;
     }
-    
 }
 
 @end
