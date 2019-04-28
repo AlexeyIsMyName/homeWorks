24c7fbc9462313a56fdccfe150cdf7d960487f39
Aleksey <suslov.aleksey.oo7@gmail.com>
2019-04-28 10:45:49 +0300

    saving changes

--- homework10(Notifications)/homework10(Notification)/homework10(Notification)/AppDelegate.m
+++ homework10(Notifications)/homework10(Notification)/homework10(Notification)/AppDelegate.m
@@ -134,9 +134,7 @@
 }
 
 - (void) governmentNotification:(NSNotification*) notification {
-    
-    //NSLog(@"governmentNotification userInfo = %@", notification.userInfo);
-    
+    NSLog(@"governmentNotification userInfo = %@", notification.userInfo);
 }
 
 - (void) dealloc {
@@ -152,11 +150,13 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    NSLog(@"applicationDidEnterBackground - METHOD");
 }
 
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    NSLog(@"applicationWillEnterForeground - METHOD");
 }
 
 
