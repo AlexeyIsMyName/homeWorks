b20f6d7e147e3b2ec8b76109e94505f914c49d6b
Aleksey <suslov.aleksey.oo7@gmail.com>
2019-05-04 21:11:13 +0300

    start new hw12

---
+++ homework12(Blocks)/homework12(Blocks)/homework12(Blocks)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  homework12(Blocks)
+//
+//  Created by Алексей on 04/05/2019.
+//  Copyright © 2019 Aleksey Suslov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
