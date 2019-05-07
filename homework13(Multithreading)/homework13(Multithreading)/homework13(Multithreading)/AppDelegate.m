8a0c7a67c5365296df02b42f3a56a3b1e7a9f236
Aleksey <suslov.aleksey.oo7@gmail.com>
2019-05-08 00:21:13 +0300

    Schoolboy's level was done!

--- homework13(Multithreading)/homework13(Multithreading)/homework13(Multithreading)/AppDelegate.m
+++ homework13(Multithreading)/homework13(Multithreading)/homework13(Multithreading)/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Student.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,66 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    /*
+     Ученик.
+     1! Создайте класс студент. У него должен быть метод - угадать ответ :)
+     2. В метод передается случайное целое число например в диапазоне от 0 - 100 (или больше) и сам диапазон, чтобы студент знал где угадывать
+     3! Студент генерирует случайное число в том же диапазоне пока оно не будет искомым
+     4! Весь процесс угадывания реализуется в потоке в классе студент
+     5! Когда студент досчитал то пусть пишет в НСЛог
+     6! Создайте 5 студентов и дайте им одну и туже задачу и посмотрите кто справился с ней лучше
+    */
+    
+    Student *student1 = [[Student alloc] initWithName:@"Student1"];
+    Student *student2 = [[Student alloc] initWithName:@"Student1"];
+    Student *student3 = [[Student alloc] initWithName:@"Student1"];
+    Student *student4 = [[Student alloc] initWithName:@"Student1"];
+    Student *student5 = [[Student alloc] initWithName:@"Student1"];
+    
+    NSArray *students = [[NSArray alloc] initWithObjects:
+                         student1,
+                         student2,
+                         student3,
+                         student4,
+                         student5,
+                         nil];
+    
+    double startTime = CACurrentMediaTime();
+    
+    for (Student *student in students) {
+        [student guessAnswer:arc4random() % 100 range:NSMakeRange(0, 100)];
+    }
+    
+    double totalTime = CACurrentMediaTime() - startTime;
+    
+    NSLog(@"totalTime - %f", totalTime);
+
+    /*
+     Студент.
+     7. Задача та же, но вместе с условием передавайте студенту блок, в котором вы и объявите результаты
+     8. Блок должен определяться в томже классе, где и определялись студенты
+     9. Блок должен быть вызван на главном потоке
+    */
+    
+    
+    
+    
+    /*
+     
+     Мастер.
+     
+     10. Создать приватный метод класса (да да, приватный метод да еще и с плюсом), который будет возвращать статическую (то есть одну на все объекты класса студент) dispatch_queue_t, которая инициализируется при первом обращении к этому методу.
+     11. Лучше в этом методе реализовать блок dispatch_once, ищите в инете как и зачем :) А что, программист всегда что-то ищет и хороший программист всегда находит.
+     12. Все студенты должны выполнять свои процессы в этой queue и она должна быть CONCURRENT, типа все блоки одновременно выполняются
+     
+     Супермен.
+     
+     13. Добавьте еще один класс студента, который делает все тоже самое что вы реализовали до этого, только вместо GCD он использует NSOperation и NSOperationQueue. Вообще вынос мозга в самостоятельной работе :)
+     14. Все сделавшие Мастера и Супермена и с красивым кодом получают отдельный огромный РЕСПЕКТ, так как они это на самом деле заслуживают.
+    
+    */
+    
     return YES;
 }
 
