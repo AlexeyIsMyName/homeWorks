70e6ebc25d20d9f507acb48f2a1f1e7003c0cf2b
Aleksey <suslov.aleksey.oo7@gmail.com>
2019-05-07 14:45:52 +0300

    13th homework was starting

---
+++ homework13(Multithreading)/homework13(Multithreading)/homework13(Multithreading)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  homework13(Multithreading)
+//
+//  Created by Алексей on 07/05/2019.
+//  Copyright © 2019 Aleksey Suslov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ homework13(Multithreading)/homework13(Multithreading)/homework13(Multithreading)/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  homework13(Multithreading)
+//
+//  Created by Алексей on 07/05/2019.
+//  Copyright © 2019 Aleksey Suslov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
