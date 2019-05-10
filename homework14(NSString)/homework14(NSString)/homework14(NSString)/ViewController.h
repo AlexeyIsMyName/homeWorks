f459dce4aa441cec89dad1aed94fbd5aa5cfceef
Aleksey <suslov.aleksey.oo7@gmail.com>
2019-05-10 18:37:01 +0300

    Starting new hw14

---
+++ homework14(NSString)/homework14(NSString)/homework14(NSString)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  homework14(NSString)
+//
+//  Created by Алексей on 10/05/2019.
+//  Copyright © 2019 Aleksey Suslov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
