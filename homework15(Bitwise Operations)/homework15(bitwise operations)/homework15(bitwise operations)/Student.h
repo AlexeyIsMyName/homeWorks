7487249810ecef6d081c4d8b7e06cee2d099af12
Aleksey <suslov.aleksey.oo7@gmail.com>
2019-05-12 12:38:40 +0300

    schoolboy level is done!

---
+++ homework15(Bitwise Operations)/homework15(bitwise operations)/homework15(bitwise operations)/Student.h
@@ -0,0 +1,31 @@
+//
+//  Student.h
+//  homework15(bitwise operations)
+//
+//  Created by Алексей on 12/05/2019.
+//  Copyright © 2019 Aleksey Suslov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef enum {
+    
+    StudentSubjectTypeBiology       = 1 << 0,
+    StudentSubjectTypeMath          = 1 << 1,
+    StudentSubjectTypeDevelopment   = 1 << 2,
+    StudentSubjectTypeEngineering   = 1 << 3,
+    StudentSubjectTypeArt           = 1 << 4,
+    StudentSubjectTypePhecology     = 1 << 5,
+    StudentSubjectTypeAnatomy       = 1 << 6
+    
+} StudentSubjectType;
+
+@interface Student : NSObject
+
+@property (assign, nonatomic) StudentSubjectType subjectType;
+
+@end
+
+NS_ASSUME_NONNULL_END
