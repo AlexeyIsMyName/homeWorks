23b570e491995ed60abe72ecfafb41c21cf39a41
Aleksey <suslov.aleksey.oo7@gmail.com>
2019-05-22 20:13:12 +0300

    start new homework 19

---
+++ homework19(UIViewGeometry)/homework19(UIViewGeometry)/homework19(UIViewGeometry)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  homework19(UIViewGeometry)
+//
+//  Created by Алексей on 22/05/2019.
+//  Copyright © 2019 Aleksey Suslov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
