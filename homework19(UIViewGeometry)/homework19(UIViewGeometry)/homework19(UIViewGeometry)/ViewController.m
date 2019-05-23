c739e1770f3811af98b2a267bc7286abb60f8bf4
Aleksey <suslov.aleksey.oo7@gmail.com>
2019-05-24 00:48:59 +0300

    super puper task is complete

--- homework19(UIViewGeometry)/homework19(UIViewGeometry)/homework19(UIViewGeometry)/ViewController.m
+++ homework19(UIViewGeometry)/homework19(UIViewGeometry)/homework19(UIViewGeometry)/ViewController.m
@@ -13,9 +13,9 @@
 @property (assign, nonatomic) CGPoint middlePoint;
 @property (assign, nonatomic) double sizeCell;
 @property (assign, nonatomic) double sizeFigure;
+@property (strong, nonatomic) NSMutableArray *figuresArray;
+@property (strong, nonatomic) UIView *mainView;
 
-
-- (CGPoint) makeFirstPoint;
 - (CGRect) makeMainRect;
 
 @end
@@ -36,13 +36,13 @@
     
     CGRect mainRect = [self makeMainRect];
     
-    UIView *mainView = [[UIView alloc] initWithFrame:mainRect];
-    mainView.backgroundColor = [UIColor whiteColor];
+    self.mainView = [[UIView alloc] initWithFrame:mainRect];
+    self.mainView.backgroundColor = [UIColor whiteColor];
     
-    mainView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleLeftMargin |
+    self.mainView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleLeftMargin |
     UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleRightMargin;
     
-    [self.view addSubview:mainView];
+    [self.view addSubview:self.mainView];
     
     NSInteger x = self.sizeCell;
     NSInteger y = 0;
@@ -53,7 +53,7 @@
             cellRect = CGRectMake(x, y, self.sizeCell, self.sizeCell);
             UIView *cellView = [[UIView alloc] initWithFrame:cellRect];
             cellView.backgroundColor = [UIColor blackColor];
-            [mainView addSubview:cellView];
+            [self.mainView addSubview:cellView];
             x = x + (self.sizeCell * 2);
         }
         y = y + self.sizeCell;
@@ -63,8 +63,10 @@
     /*
      Супермен
      8! Поставьте белые и красные шашки (квадратные вьюхи) так как они стоят на доске. Они должны быть сабвьюхами главной вьюхи (у них и у клеток один супервью)
-     9. После каждого переворота шашки должны быть перетасованы используя соответствующие методы иерархии UIView
-     */
+     9! После каждого переворота шашки должны быть перетасованы используя соответствующие методы иерархии UIView
+    */
+    
+    self.figuresArray = [[NSMutableArray alloc] init];
     
     double ratioFigureByCell = 0.6; // ready
     double different = (self.sizeCell - self.sizeCell * ratioFigureByCell) / 2; // ready
@@ -80,7 +82,6 @@
             
             figureRect = CGRectMake(x + different, y + different, self.sizeFigure, self.sizeFigure);
             UIView *figureView = [[UIView alloc] initWithFrame:figureRect];
-            figureView.backgroundColor = [UIColor redColor];
             
             figureView.autoresizingMask =   UIViewAutoresizingFlexibleTopMargin |
                                             UIViewAutoresizingFlexibleLeftMargin |
@@ -88,11 +89,15 @@
                                             UIViewAutoresizingFlexibleRightMargin;
             
             if (((indexX % 2) != (indexY % 2)) && indexX < 3) {
-                [mainView addSubview:figureView];
+                figureView.backgroundColor = [UIColor redColor];
+                [self.mainView addSubview:figureView];
                 x = x + (self.sizeCell * 2);
+                [self.figuresArray addObject:figureView];
             } else if (((indexX % 2) != (indexY % 2)) && indexX > 6) {
-                [mainView addSubview:figureView];
+                figureView.backgroundColor = [UIColor blueColor];
+                [self.mainView addSubview:figureView];
                 x = x + (self.sizeCell * 2);
+                [self.figuresArray addObject:figureView];
             }
     
         }
@@ -101,7 +106,7 @@
     }
 }
 
-- (UIInterfaceOrientationMask) supportedInterfaceOrientation {
+- (UIInterfaceOrientationMask) supportedInterfaceOrientations {
     return UIInterfaceOrientationMaskAll;
 }
 
@@ -141,23 +146,22 @@
             }
         }
     }
-}
-
-/*
- Супермен
- 8! Поставьте белые и красные шашки (квадратные вьюхи) так как они стоят на доске. Они должны быть сабвьюхами главной вьюхи (у них и у клеток один супервью)
- 9. После каждого переворота шашки должны быть перетасованы используя соответствующие методы иерархии UIView
-*/
-
-- (CGPoint) makeFirstPoint {
-    NSInteger x = 0;
-    NSInteger y = 0;
-    if (self.view.bounds.size.height > self.view.bounds.size.width) {
-        y = (self.view.bounds.size.height - self.view.bounds.size.width) / 2;
-    } else {
-        x = (self.view.bounds.size.width - self.view.bounds.size.height) / 2;
+    
+    NSInteger randomCountChange = arc4random() % ([self.figuresArray count] / 2) + 1;
+    
+    for (NSInteger i = 0; i < randomCountChange; i++) {
+        UIView *firstFigure = [self.figuresArray objectAtIndex:arc4random() % [self.figuresArray count]];
+        UIView *secondFigure = [self.figuresArray objectAtIndex:arc4random() % [self.figuresArray count]];
+        
+        [UIView animateWithDuration:1 animations:^{
+            CGRect tempRect = firstFigure.frame;
+            [firstFigure setFrame:secondFigure.frame];
+            [secondFigure setFrame:tempRect];
+            
+            [self.mainView bringSubviewToFront:firstFigure];
+            [self.mainView bringSubviewToFront:secondFigure];
+        }];
     }
-    return CGPointMake(x, y);
 }
 
 - (CGRect) makeMainRect {
@@ -175,6 +179,5 @@
     
     return CGRectMake(x, y, size, size);
 }
-
 
 @end
