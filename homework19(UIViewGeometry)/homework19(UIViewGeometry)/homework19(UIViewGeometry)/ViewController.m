55821d212fead2248c1c5dac8433061ed1f54eac
Aleksey <suslov.aleksey.oo7@gmail.com>
2019-05-23 18:53:57 +0300

    Master level is done.

--- homework19(UIViewGeometry)/homework19(UIViewGeometry)/homework19(UIViewGeometry)/ViewController.m
+++ homework19(UIViewGeometry)/homework19(UIViewGeometry)/homework19(UIViewGeometry)/ViewController.m
@@ -27,16 +27,20 @@
      Ученик
      1! В цикле создавайте квадратные UIView с черным фоном и расположите их в виде шахматной доски
      2! доска должна иметь столько клеток, как и настоящая шахматная
+     Студент
+     3! Доска должна быть вписана в максимально возможный квадрат, т.е. либо бока, либо верх или низ должны касаться границ экрана
+     4! Применяя соответствующие маски сделайте так, чтобы когда устройство меняет ориентацию, то все клетки растягивались соответственно и ничего не вылетало за пределы экрана.
     */
     
     CGRect mainRect = [self makeMainRect];
     
-    UIView *mainWhiteView = [[UIView alloc] initWithFrame:mainRect];
-    mainWhiteView.backgroundColor = [UIColor whiteColor];
-    mainWhiteView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleLeftMargin |
+    UIView *mainView = [[UIView alloc] initWithFrame:mainRect];
+    mainView.backgroundColor = [UIColor whiteColor];
+    
+    mainView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleLeftMargin |
     UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleRightMargin;
     
-    [self.view addSubview:mainWhiteView];
+    [self.view addSubview:mainView];
     
     NSInteger x = self.sizeCell;
     NSInteger y = 0;
@@ -45,9 +49,9 @@
     for (int indexY = 0; indexY < 8; indexY++) {
         for (int indexX = 0; indexX < 4; indexX++) {
             cellRect = CGRectMake(x, y, self.sizeCell, self.sizeCell);
-            UIView *cellBlackView = [[UIView alloc] initWithFrame:cellRect];
-            cellBlackView.backgroundColor = [UIColor blackColor];
-            [mainWhiteView addSubview:cellBlackView];
+            UIView *cellView = [[UIView alloc] initWithFrame:cellRect];
+            cellView.backgroundColor = [UIColor blackColor];
+            [mainView addSubview:cellView];
             x = x + (self.sizeCell * 2);
         }
         y = y + self.sizeCell;
@@ -59,27 +63,45 @@
     return UIInterfaceOrientationMaskAll;
 }
 
-- (void) didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation {
-    
-    //NSLog(@"height = %f, width = %f", self.view.bounds.size.height, self.view.bounds.size.width);
-    
-}
-
-
 /*
-Студент
-3! Доска должна быть вписана в максимально возможный квадрат, т.е. либо бока, либо верх или низ должны касаться границ экрана
-4. Применяя соответствующие маски сделайте так, чтобы когда устройство меняет ориентацию, то все клетки растягивались соответственно и ничего не вылетало за пределы экрана.
-
-Мастер
-5. При повороте устройства все черные клетки должны менять цвет :)
-6. Сделайте так, чтобы доска при поворотах всегда строго находилась по центру
-
-Супермен
-8. Поставьте белые и красные шашки (квадратные вьюхи) так как они стоят на доске. Они должны быть сабвьюхами главной вьюхи (у них и у клеток один супервью)
-9. После каждого переворота шашки должны быть перетасованы используя соответствующие методы иерархии UIView
+ Мастер
+ 5! При повороте устройства все черные клетки должны менять цвет :)
+ 6! Сделайте так, чтобы доска при поворотах всегда строго находилась по центру
 */
 
+- (void) viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
+    
+    UIInterfaceOrientation interfaceOrientation = [[UIApplication sharedApplication] statusBarOrientation];
+    
+    if (UIInterfaceOrientationIsPortrait(interfaceOrientation)) {
+        UIView *mainView = [self.view.subviews objectAtIndex:0];
+        UIColor *color = [UIColor colorWithRed:0.8
+                                         green:0.8
+                                          blue:1
+                                         alpha:1];
+        
+        mainView.backgroundColor = color;
+        
+        for (UIView *cellView in mainView.subviews) {
+            cellView.backgroundColor = [UIColor brownColor];
+        }
+    }
+    
+    if (UIInterfaceOrientationIsLandscape(interfaceOrientation)) {
+        UIView *mainView = [self.view.subviews objectAtIndex:0];
+        mainView.backgroundColor = [UIColor whiteColor];
+        
+        for (UIView *cellView in mainView.subviews) {
+            cellView.backgroundColor = [UIColor blackColor];
+        }
+    }
+}
+
+/*
+ Супермен
+ 8. Поставьте белые и красные шашки (квадратные вьюхи) так как они стоят на доске. Они должны быть сабвьюхами главной вьюхи (у них и у клеток один супервью)
+ 9. После каждого переворота шашки должны быть перетасованы используя соответствующие методы иерархии UIView
+*/
 
 - (CGPoint) makeFirstPoint {
     NSInteger x = 0;
