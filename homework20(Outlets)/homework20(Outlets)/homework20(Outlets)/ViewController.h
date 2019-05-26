2396ca4d7dbac43d207d751c44e17223e0f693f5
Aleksey <suslov.aleksey.oo7@gmail.com>
2019-05-26 18:12:51 +0300

    create homework20

---
+++ homework20(Outlets)/homework20(Outlets)/homework20(Outlets)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  homework20(Outlets)
+//
+//  Created by Алексей on 26/05/2019.
+//  Copyright © 2019 Aleksey Suslov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ homework20(Outlets)/homework20(Outlets)/homework20(Outlets)/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  homework20(Outlets)
+//
+//  Created by Алексей on 26/05/2019.
+//  Copyright © 2019 Aleksey Suslov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
