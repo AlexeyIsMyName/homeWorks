8a9eb3a6cc19e89aa078e890960b3a62d1ec3632
Aleksey <suslov.aleksey.oo7@gmail.com>
2019-05-31 23:12:24 +0300

    start hw21 in swift

---
+++ homework21(UIView Animations)/homework21(Animations)/homework21(Animations)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework21(Animations)
+//
+//  Created by Алексей on 31/05/2019.
+//  Copyright © 2019 Aleksey Suslov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
