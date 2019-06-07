5ee16f86738238a919302282917d7de45d561ab3
Aleksey <suslov.aleksey.oo7@gmail.com>
2019-06-07 13:56:23 +0300

    start hw22

---
+++ homework22/homework22/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework22
+//
+//  Created by Алексей on 07/06/2019.
+//  Copyright © 2019 Aleksey Suslov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
