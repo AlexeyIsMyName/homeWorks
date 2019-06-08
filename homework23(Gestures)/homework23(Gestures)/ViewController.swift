e09179a8bb6ed6e3840e3b789dd4a975149e9941
Aleksey <suslov.aleksey.oo7@gmail.com>
2019-06-08 18:02:08 +0300

    start homework23

---
+++ homework23(Gestures)/homework23(Gestures)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework23(Gestures)
+//
+//  Created by Алексей on 08/06/2019.
+//  Copyright © 2019 Aleksey Suslov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
