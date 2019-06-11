593317cfc8a06cce3aed3eae4e25c192fe17d335
Aleksey <suslov.aleksey.oo7@gmail.com>
2019-06-11 17:55:40 +0300

    saving changes

--- homework23(Gestures)/homework23(Gestures)/ViewController.swift
+++ homework23(Gestures)/homework23(Gestures)/ViewController.swift
@@ -22,8 +22,8 @@ class ViewController: UIViewController {
      4! Если я вдруг делаю тач во время анимации, то картинка должна двигаться в новую точку без рывка (как будто она едет себе и все)
      
      Мастер
-     5. Если я делаю свайп вправо, то давайте картинке анимацию поворота по часовой стрелке на 360 градусов
-     6. То же самое для свайпа влево, только анимация должна быть против часовой (не забудьте остановить предыдущее кручение)
+     5! Если я делаю свайп вправо, то давайте картинке анимацию поворота по часовой стрелке на 360 градусов
+     6! То же самое для свайпа влево, только анимация должна быть против часовой (не забудьте остановить предыдущее кручение)
      7. По двойному тапу двух пальцев останавливайте анимацию
      
      Супермен
@@ -34,6 +34,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let image = UIImage.init(named: "maincable_midspan.jpeg");
+        let imageView = UIImageView.init(image: image);
+        
+        let viewWithImage = UIView.init(frame: CGRect(x: 0, y: 0, width: 768, height: 1024));
+        viewWithImage.addSubview(imageView);
+        self.view.addSubview(viewWithImage);
         
         let rectWidth: CGFloat = 100.0;
         let rectHeight: CGFloat = 100.0;
@@ -47,23 +54,83 @@ class ViewController: UIViewController {
         self.view.addSubview(view);
         
         //UIGestureRecognizer
-        //One tap
+        //One tap Gesture
         let tapGesture = UITapGestureRecognizer.init(target: self, action: #selector(self.handleTap(_:)));
         self.view.addGestureRecognizer(tapGesture);
+        
+        //Swipe Gesture
+        let verticalSwipeGesture = UISwipeGestureRecognizer.init(target: self, action: #selector(self.handleVertialSwipe(_:)));
+        let horizontalSwipeGesture = UISwipeGestureRecognizer.init(target: self, action: #selector(self.handleHorizontalSwipe(_:)));
+        verticalSwipeGesture.direction = [.down, .up];
+        horizontalSwipeGesture.direction = [.left, .right];
+        self.view.addGestureRecognizer(verticalSwipeGesture);
+        self.view.addGestureRecognizer(horizontalSwipeGesture);
+        
+        //Double tap & Double touch Gesture
+        let doubleTapDoubleTouchGesture = UITapGestureRecognizer.init(target: self, action: #selector(self.handleDoubleTapDoubleTouch(_:)));
+        doubleTapDoubleTouchGesture.numberOfTapsRequired = 2;
+        doubleTapDoubleTouchGesture.numberOfTouchesRequired = 2;
+        self.view.addGestureRecognizer(doubleTapDoubleTouchGesture);
     }
     
     @objc func handleTap(_ tapGesture: UITapGestureRecognizer) {
         print(tapGesture.location(in: self.view));
-        UIView.animate(withDuration: 3,
+        UIView.animate(withDuration: 5,
                         delay: 0,
-                        options: [UIView.AnimationOptions.beginFromCurrentState],
+                        options: [.beginFromCurrentState],
                         animations: {
-                            self.testView.backgroundColor = UIColor.green;
+                            self.testView.backgroundColor = self.randomColor();
                             self.testView.center = tapGesture.location(in: self.view);
                         }) { (Bool) in
                         };
     }
     
+    @objc func handleVertialSwipe(_ verticalSwipeGesture: UISwipeGestureRecognizer) {
+        let currentTransform = self.testView.transform;
+        let newTransform = currentTransform.rotated(by: 3.14159);
+        UIView.animate(withDuration: 5,
+                       delay: 0,
+                       options: [.beginFromCurrentState],
+                       animations: {
+                        self.testView.backgroundColor = UIColor.black;
+                        self.testView.alpha = 1.0;
+                        self.testView.transform = newTransform;
+        }) { (Bool) in
+        };
+    }
+    
+    @objc func handleHorizontalSwipe(_ horizontalSwipeGesture: UISwipeGestureRecognizer) {
+        let currentTransform = self.testView.transform;
+        let newTransform = currentTransform.rotated(by: -3.14159);
+        UIView.animate(withDuration: 5,
+                       delay: 0,
+                       options: [.beginFromCurrentState],
+                       animations: {
+                        self.testView.backgroundColor = UIColor.white;
+                        self.testView.alpha = 1.0;
+                        self.testView.transform = newTransform;
+        }) { (Bool) in
+        };
+    }
+    
+    @objc func handleDoubleTapDoubleTouch(_ tapGesture: UISwipeGestureRecognizer) {
+        UIView.animate(withDuration: 2,
+                       delay: 0,
+                       options: [.beginFromCurrentState],
+                       animations: {
+                        self.testView.backgroundColor = self.randomColor();
+                        self.testView.alpha = 1.0;
+        }) { (Bool) in
+        };
+    }
+    
+    func randomColor () -> (UIColor) {
+        let r = CGFloat.random(in: 0...256) / 255;
+        let g = CGFloat.random(in: 0...256) / 255;
+        let b = CGFloat.random(in: 0...256) / 255;
+        let randomAlpha = CGFloat.random(in: 0.6...0.9);
+        return UIColor.init(red: r, green: g, blue: b, alpha: randomAlpha);
+    }
     
 }
 
