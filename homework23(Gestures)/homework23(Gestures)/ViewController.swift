c3ccaa38c32c47eb889bc299a4e98c94d8b4005f
Aleksey <suslov.aleksey.oo7@gmail.com>
2019-06-12 20:02:00 +0300

    Task is completed

--- homework23(Gestures)/homework23(Gestures)/ViewController.swift
+++ homework23(Gestures)/homework23(Gestures)/ViewController.swift
@@ -11,6 +11,15 @@ import UIKit
 class ViewController: UIViewController {
     
     var testView = UIView();
+    var testViewScale = CGFloat();
+    var testViewRotation = CGFloat();
+    
+    enum TestViewColorCheck {
+         case up
+         case down
+    }
+    
+    var testViewColorCheck = TestViewColorCheck.up;
     
     /*
      Ученик
@@ -24,11 +33,11 @@ class ViewController: UIViewController {
      Мастер
      5! Если я делаю свайп вправо, то давайте картинке анимацию поворота по часовой стрелке на 360 градусов
      6! То же самое для свайпа влево, только анимация должна быть против часовой (не забудьте остановить предыдущее кручение)
-     7. По двойному тапу двух пальцев останавливайте анимацию
+     7! По двойному тапу двух пальцев останавливайте анимацию
      
      Супермен
-     8. Добавьте возможность зумить и отдалять картинку используя пинч
-     9. Добавьте возможность поворачивать картинку используя ротейшн
+     8! Добавьте возможность зумить и отдалять картинку используя пинч
+     9! Добавьте возможность поворачивать картинку используя ротейшн
     */
     
     override func viewDidLoad() {
@@ -71,6 +80,15 @@ class ViewController: UIViewController {
         doubleTapDoubleTouchGesture.numberOfTapsRequired = 2;
         doubleTapDoubleTouchGesture.numberOfTouchesRequired = 2;
         self.view.addGestureRecognizer(doubleTapDoubleTouchGesture);
+        
+        //Pinch
+        let pinchGesure = UIPinchGestureRecognizer.init(target: self, action: #selector(self.handlePinch(_:)));
+        self.testViewScale = pinchGesure.scale;
+        self.view.addGestureRecognizer(pinchGesure);
+        
+        //Rotation
+        let rotationGesure = UIRotationGestureRecognizer.init(target: self, action: #selector(self.handleRotation(_:)));
+        self.view.addGestureRecognizer(rotationGesure);
     }
     
     @objc func handleTap(_ tapGesture: UITapGestureRecognizer) {
@@ -114,14 +132,35 @@ class ViewController: UIViewController {
     }
     
     @objc func handleDoubleTapDoubleTouch(_ tapGesture: UISwipeGestureRecognizer) {
-        UIView.animate(withDuration: 2,
-                       delay: 0,
-                       options: [.beginFromCurrentState],
-                       animations: {
-                        self.testView.backgroundColor = self.randomColor();
-                        self.testView.alpha = 1.0;
-        }) { (Bool) in
-        };
+        self.testView.layer.removeAllAnimations();
+    }
+    
+    @objc func handlePinch(_ pinchGesture: UIPinchGestureRecognizer) {
+        
+        if pinchGesture.state == UIGestureRecognizer.State.began {
+            self.testViewScale = 1.0;
+        }
+        
+        let newScale: CGFloat = 1.0 + pinchGesture.scale - self.testViewScale;
+        let currentTransform = self.testView.transform;
+        let newTransform = CGAffineTransform.init(scaleX: newScale, y: newScale)
+        self.testView.transform = newTransform.concatenating(currentTransform);
+        self.testViewScale = pinchGesture.scale;
+        self.changeColor();
+    }
+    
+    @objc func handleRotation(_ rotationGesture: UIRotationGestureRecognizer) {
+        
+        if rotationGesture.state == UIRotationGestureRecognizer.State.began {
+            self.testViewRotation = 0.0;
+        }
+        
+        let newRotation = rotationGesture.rotation - self.testViewRotation;
+        let currentTransform = self.testView.transform;
+        let newTransform = CGAffineTransform.init(rotationAngle: newRotation);
+        self.testView.transform = newTransform.concatenating(currentTransform);
+        self.testViewRotation = rotationGesture.rotation;
+        self.changeColor();
     }
     
     func randomColor () -> (UIColor) {
@@ -132,5 +171,53 @@ class ViewController: UIViewController {
         return UIColor.init(red: r, green: g, blue: b, alpha: randomAlpha);
     }
     
+    func changeColor () {
+        let currentBackgroundColor = self.testView.backgroundColor;
+        
+        var r: CGFloat = 0.0;
+        var g: CGFloat = 0.0;
+        var b: CGFloat = 0.0;
+        var a: CGFloat = 0.0;
+        let delta: CGFloat = 0.025;
+        
+        currentBackgroundColor?.getRed(&r, green: &g, blue: &b, alpha: &a);
+        
+        if self.testViewColorCheck == .up {
+            if r < 1.0 {
+                r = r + delta;
+            }
+            
+            if r >= 1.0 && g < 1.0 {
+                g = g + delta;
+            }
+            
+            if g >= 1.0 && b < 1.0 {
+                b = b + delta;
+            }
+            
+            if r >= 1.0 && g >= 1.0 && b >= 1.0 {
+                self.testViewColorCheck = .down;
+            }
+        }
+        
+        if self.testViewColorCheck == .down {
+            if r > 0.0 {
+                r = r - delta;
+            }
+            
+            if r <= 0.0 && g > 0.0 {
+                g = g - delta;
+            }
+            
+            if g <= 0.0 && b > 0.0 {
+                b = b - delta;
+            }
+            
+            if r <= 0.0 && g <= 0.0 && b <= 0.0 {
+                self.testViewColorCheck = .up;
+            }
+        }
+        self.testView.backgroundColor = UIColor.init(red: r, green: g, blue: b, alpha: 0.9);
+    }
 }
 
