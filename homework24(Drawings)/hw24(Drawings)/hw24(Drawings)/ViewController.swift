37c3de694c077dce10cb89295731c8a6242a503b
Aleksey <suslov.aleksey.oo7@gmail.com>
2019-06-15 15:42:01 +0300

    homework 24. first commit
    start next task about drawings in swift

---
+++ homework24(Drawings)/hw24(Drawings)/hw24(Drawings)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hw24(Drawings)
+//
+//  Created by Алексей on 15/06/2019.
+//  Copyright © 2019 Aleksey Suslov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
